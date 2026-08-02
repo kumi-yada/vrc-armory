17bc0d9f1819e606fc1bc5b4236495369df9067e
myin <minyin142p@gmail.com>
2026-08-02 12:59:23 +0200

    improve pickup

---
+++ Blacksmith/Scripts/ImprovedPickup.cs
@@ -0,0 +1,28 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDK3.Components;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class ImprovedPickup : UdonSharpBehaviour
+{
+    void Start()
+    {
+        var pickup = GetComponent<VRCPickup>();
+        pickup.pickupable = Networking.IsOwner(gameObject);
+
+        var owner = Networking.GetOwner(gameObject);
+        if (owner.IsUserInVR())
+        {
+            pickup.AutoHold = VRC_Pickup.AutoHoldMode.No;
+            pickup.orientation = VRC_Pickup.PickupOrientation.Any;
+        }
+        else
+        {
+            pickup.AutoHold = VRC_Pickup.AutoHoldMode.Yes;
+            pickup.orientation = VRC_Pickup.PickupOrientation.Grip;
+        }
+    }
+
+}
