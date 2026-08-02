0e2c797df59b7f639981af75aeb13990ae963225
myin <minyin142p@gmail.com>
2026-08-02 22:12:09 +0200

    exp 1000

--- Blacksmith/Scripts/BlacksmithData.cs
+++ Blacksmith/Scripts/BlacksmithData.cs
@@ -5,16 +5,17 @@ public static class BlacksmithData
     public const string EXP_KEY = "blacksmith_exp";
     public const string GOLD_KEY = "blacksmith_gold";
     public const string FORGE_LEVEL_KEY = "blacksmith_forge_level";
+    public const float EXP_PER_LEVEL = 1000f;
 
     public static int GetLevel(float exp)
     {
-        return Mathf.FloorToInt(exp / 100f) + 1;
+        return Mathf.FloorToInt(exp / EXP_PER_LEVEL) + 1;
     }
 
     public static float ExpForLevel(int level)
     {
         int l = Mathf.Max(1, level) - 1;
-        return l * 100f;
+        return l * EXP_PER_LEVEL;
     }
 
     public static float ProgressToNextLevel(float exp)
